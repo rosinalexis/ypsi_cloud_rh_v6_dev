360f8aee5f352a76d8d674b593e158799a2baa06
Alexis ANDRIANASY <alexis.andrianasy@ypsi.fr>
2022-07-24 23:20:06 +0200

    ADD -- Entity Category
    Ajout de l'entité category et d'un trait

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return new JsonResponse(
+            [
+                'action' => 'index',
+                'time' => time()
+            ],
+
+        );
+    }
+}
---
+++ src/Entity/Traits/Timestamplable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait Timestamplable
+{
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, options: ["default" => "CURRENT_TIMESTAMP"])]
+    private ?\DateTimeImmutable $createdAt = null ;
+
+
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, options: ["default" => "CURRENT_TIMESTAMP"])]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt = null;
+    }
+
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+
+    public function setUpdatedAt($updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    #[
+        ORM\PrePersist,
+        ORM\PreUpdate
+    ]
+    public function updateTimestamps(): void
+    {
+        if ($this->getCreatedAt() === null) {
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+
+        $this->setUpdatedAt(new \DateTimeImmutable);
+    }
+}
